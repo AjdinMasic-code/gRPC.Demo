e63daa5632e1a096fb41eb9af3d97ea791188763
Ajdin Masic <ajdin.masic@atmosoft.com>
2023-06-19 21:49:38 -0600

    Add project files.

---
+++ gRPC.Client.Service/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using gRPC.Demo.Client;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7034");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine($"greetings: {reply.Message}");
+Console.WriteLine("Press any key to exit....");
+
+Console.ReadLine();
